ea3b04a310c6c585cfcbb196f253110424e67c04
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-01-11 13:10:33 +0900

    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:user_remember_token] = remember_token
-    user.update!(remember_token: User.encrypt(remember_token))
+    user.update_attribute!(remember_token: User.encrypt(remember_token))
     @current_user = user
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :university, :faculty, :image)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :university, :faculty, :image, :remember_token)
     end
 
 end
